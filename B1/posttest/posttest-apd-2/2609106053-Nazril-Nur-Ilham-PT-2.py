e0d6ef55aa669165e4a9f7da483be2606308a2be
yoshril <nazrilaya3@gmail.com>
2026-09-21 17:41:39 +0800

    posttest 2

---
+++ B1/posttest/posttest-apd-2/2609106053-Nazril-Nur-Ilham-PT-2.py
@@ -0,0 +1,31 @@
+makanan_1 = 15000
+makanan_2 = 16000
+makanan_3 = 19000
+makanan_4 = 20000
+makanan_5 = 21000
+makanan_6 = 22000
+
+harga_makanan = [makanan_1, makanan_2, makanan_3, makanan_4, makanan_5, makanan_6]
+
+total_bayar = makanan_1 + makanan_2 + makanan_3 + makanan_4 + makanan_5 + makanan_6 + 5000
+
+konversi_euro = total_bayar / 20359
+
+rata_rata = total_bayar / len(harga_makanan)
+
+nim = 53
+
+bolean = nim < rata_rata
+
+print("Makanan 1 : Rp." + str(makanan_1))
+print("Makanan 2 : Rp." + str(makanan_2))
+print("Makanan 3 : Rp." + str(makanan_3))
+print("Makanan 4 : Rp." + str(makanan_4))
+print("Makanan 5 : Rp." + str(makanan_5))
+print("Makanan 6 : Rp." + str(makanan_6))
+print("Total Bayar : Rp." + str(total_bayar))
+print("NIM : " + str(nim))
+print("Bolean : " + str(bolean))
+print("Total Bayar Dalam Euro : €" + str(konversi_euro))
+print("Isi Makanan 3 - 5 : " + str(harga_makanan[-4:-1]))
+
---
+++ pertemuan1.py
@@ -0,0 +1,83 @@
+# print("ini mau saya push")
+
+# #numerik
+
+# angka1 = 5
+# angka2 = 10
+# hasil = angka1 + angka2
+# print(hasil)
+
+# #float
+# a = 3.45
+# b = 1.23
+# hitung = a + b
+# print(hitung)
+
+# #string
+# nama = "Larry"
+# hobi = "ngoding"
+# print(nama)
+# print(hobi)
+
+# print('''Halo saya Rendy
+# Saya suka belajar coding
+# Hari ini saya belajar bahasa pemrograman Python''')
+
+# print("aku""rajin""praktikum")
+# print("aku" + "rajin")
+
+# teks = "Rahmat Flowchart"
+# print(teks[3])
+# print(teks[3:6])
+# print(teks[7:])
+# print(teks[:6])
+# print(teks[1:4:2])
+# print(teks[-1])
+
+# biodata = ["Larry", 19, "2026", 82.49, True, ["APD", "26"]]
+# print(biodata)
+
+# mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+# print(mata_kuliah[0])
+# print(mata_kuliah[1])
+# print(mata_kuliah[2])
+# print(mata_kuliah[0:1])
+# print(mata_kuliah[:2])
+# print(mata_kuliah[1:])
+# print(mata_kuliah[::2])
+# print(mata_kuliah[:-1])
+
+# buku = {
+# 'judul' : 'Atomic Habits',
+# 'penulis' : 'James Clear',
+# 'halaman' : 320
+# }
+# ...
+# # Mengakses Value dalam Dictionary
+# print(buku['judul'])
+# print(buku['penulis'])
+# print(buku['halaman'])
+
+# angka = int("42")
+# print(angka)
+
+# ubah = float("99.99")
+# print(ubah)
+
+# nama = input("Masukkan nama kamu: ")
+# print (nama)
+
+# gaji = int(input("Masukkan gaji kamu: "))
+# print(gaji)
+
+# print(2 < 4)
+# print(5 > 5)
+# print(2 < 4 and 5 > 10)
+
+zidan = {
+    'pagi' : "kalkulus",
+    'siang' : "APD",
+    'malam' : "bahasa inggris"
+}
+...
+print(zidan['siang'])
